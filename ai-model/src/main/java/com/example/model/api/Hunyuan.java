799c6e48f304d5363bc316253855419c387a2ed0
paoxia <yushengyun2013@163.com>
2025-05-08 22:34:22 +0800

    引入混元大模型

--- ai-model/src/main/java/com/example/model/api/Hunyuan.java
+++ ai-model/src/main/java/com/example/model/api/Hunyuan.java
@@ -1,8 +1,27 @@
 package com.example.model.api;
 
+import com.tencentcloudapi.hunyuan.v20230901.HunyuanClient;
+
 /**
  * tencent hunyuan model
  */
 public class Hunyuan {
 
+    /**
+     * chat接口
+     *
+     * @return 聊天回答
+     */
+    public String chat() {
+        return "";
+    }
+
+    /**
+     * 翻译
+     *
+     * @return
+     */
+    public String translate() {
+        return "";
+    }
 }
--- ai-vector-db/src/main/java/com/example/ai/vectordb/dao/ElasticSearchVectorDb.java
+++ ai-vector-db/src/main/java/com/example/ai/vectordb/dao/ElasticSearchVectorDb.java
@@ -1,6 +1,8 @@
 package com.example.ai.vectordb.dao;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class ElasticSearchVectorDb {
 
 }
